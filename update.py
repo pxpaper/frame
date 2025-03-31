21a5880892800f10dd91c155335af30f3b292f99
pxpaper <pxpaper@outlook.com>
2025-03-30 19:16:50 -0700

    u

--- update.py
+++ update.py
@@ -3,11 +3,6 @@ import os
 import stat
 import subprocess
 
-# Automatically set the executable bit on this file.
-# 0o755 means: owner read/write/execute, group and others read/execute.
-current_file = os.path.realpath(__file__)
-os.chmod(current_file, 0o755)
-
 def update_repo():
     try:
         # Pull the latest changes from GitHub
